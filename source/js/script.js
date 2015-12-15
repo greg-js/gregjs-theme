6f79cc4eb4e57ca5c614529adb0bb059818d3089
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2015-12-15 15:51:57 +0000

    edited archives

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,19 @@
-(function($) {
-  // Caption
-  $('.entry-content').each(function(i) {
-    $(this).find('img').each(function() {
-      if ($(this).parent().hasClass('fancybox')) return;
+/* global jQuery */
 
+'use strict';
+
+(function runEverywhere($) {
+  // Caption
+  $('.entry-content').each(function addFancybox(i) {
+    $(this).find('img').each(function wrapIt() {
       var alt = this.alt;
+
+      if ( ($(this).parent().hasClass('fancybox')) ) { return; }
 
       $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
     });
 
-    $(this).find('.fancybox').each(function() {
+    $(this).find('.fancybox').each(function forEachFancybox() {
       $(this).attr('rel', 'article' + i);
     });
   });
@@ -19,31 +23,31 @@
   }
 
   // top nav
-  $('.nav a').first().hover(function() {
+  $('.nav a').first().hover(function hoverHome() {
     this.text = 'home';
-  }, function() {
+  }, function changeLook() {
     this.text = '../';
   });
 
-  $('.nav a:eq(1)').hover(function() {
+  $('.nav a:eq(1)').hover(function hoverBlog() {
     this.text = 'blog';
-  }, function() {
+  }, function changeLook() {
     this.text = './';
   });
 
-  $('.menu-top').on('click', '.menu-item-submenu', function(e) {
+  $('.menu-top').on('click', '.menu-item-submenu', function onClick(e) {
     var $sub = $(this).parent().find('.submenu');
     if ($sub.length) {
       e.preventDefault();
       $sub.toggleClass('active');
-      $(this).parent().on('mouseleave', function() {
+      $(this).parent().on('mouseleave', function onMouseLeave() {
         $sub.removeClass('active');
       });
     }
   });
 
   // Mobile nav
-  $('.mobile-nav-panel').click(function() {
+  $('.mobile-nav-panel').click(function onClick() {
     $('.nav').toggleClass('active');
   });
 
