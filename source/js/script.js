9afc2ecd663880450974b94ebbe7eecac15e536a
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2016-03-18 14:59:18 +0000

    updated header

--- source/js/script.js
+++ source/js/script.js
@@ -8,7 +8,7 @@
     $(this).find('img').each(function wrapIt() {
       var alt = this.alt;
 
-      if ( ($(this).parent().hasClass('fancybox')) ) { return; }
+      if (($(this).parent().hasClass('fancybox'))) { return; }
 
       $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
     });
@@ -23,11 +23,11 @@
   }
 
   // top nav
-  $('.menu-top').on('click', '.menu-item-submenu', function onClick(e) {
+  $('.menu-top').on('mouseover', '.menu-item-submenu', function onHover(e) {
     var $sub = $(this).parent().find('.submenu');
     if ($sub.length) {
       e.preventDefault();
-      $sub.toggleClass('active');
+      $sub.addClass('active');
       $(this).parent().on('mouseleave', function onMouseLeave() {
         $sub.removeClass('active');
       });
@@ -39,4 +39,4 @@
     $('.nav').toggleClass('active');
   });
 
-})(jQuery);
+}(jQuery));
