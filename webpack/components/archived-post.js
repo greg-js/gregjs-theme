626f368b1f2965e23bbbf15baf87aea7a4bdd097
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2016-09-22 16:04:33 +0100

    flexified the archive

--- webpack/components/archived-post.js
+++ webpack/components/archived-post.js
@@ -7,18 +7,20 @@ const ArchivedPost = ({ postData, setFilter }) => {
 
   return (
     <div class='archived-post'>
-      <h2><a href={url}>{title}</a></h2>
-      <div class='archived-post-metadata'>
-        <div class='archived-post-metadata-date'>
-          {formatDate(date)}
-        </div>
-        <div class='archived-post-metadata-category-div'>
-          Category: <a class='archived-post-metadata-category-item' onClick={ () => setFilter({category}) }>{category}</a>
-        </div>
-        <div class='archived-post-metadata-tags-div'>
-        Tags: {tags.map(tag => (
-          <span>#<a class='archived-post-metadata-tags-item' onClick={ () => setFilter({tag}) }>{tag}</a> </span>
-        ))}
+      <div class='archived-post-content'>
+        <h2><a href={url}>{title}</a></h2>
+        <div class='archived-post-metadata'>
+          <div class='archived-post-metadata-category-div'>
+            Category: <a class='archived-post-metadata-category-item' onClick={ () => setFilter({category}) }>{category}</a>
+          </div>
+          <div class='archived-post-metadata-tags-div'>
+            Tags: {tags.map(tag => (
+              <span>#<a class='archived-post-metadata-tags-item' onClick={ () => setFilter({tag}) }>{tag}</a> </span>
+            ))}
+          </div>
+          <div class='archived-post-metadata-date'>
+            {formatDate(date)}
+          </div>
         </div>
       </div>
     </div>
